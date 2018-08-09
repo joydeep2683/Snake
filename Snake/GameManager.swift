9fb73c50d356c2d99468f4f2c6c6c8dd9d514356
Joydeep Ballabh <boloram2683@gmail.com>
2018-08-09 15:27:07 +0530

    Built starting menu for the game

---
+++ Snake/GameManager.swift
@@ -0,0 +1,14 @@
+//
+//  GameManager.swift
+//  Snake
+//
+//  Created by Joydeep on 09/08/18.
+//  Copyright © 2018 Joydeep. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class GameManager {
+    
+}
