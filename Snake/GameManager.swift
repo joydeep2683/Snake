d97dd10d3e6baf601de89f496015d74c2636049a
Joydeep Ballabh <boloram2683@gmail.com>
2018-08-10 08:48:33 +0530

    Created the snake in the scene

--- Snake/GameManager.swift
+++ Snake/GameManager.swift
@@ -10,5 +10,36 @@ import Foundation
 import SpriteKit
 
 class GameManager {
+    var scene : GameScene!
     
+    init(scene : GameScene) {
+        self.scene = scene
+    }
+    
+    func initGame(){
+        scene.playerPosition.append((10, 10))
+        scene.playerPosition.append((10, 11))
+        scene.playerPosition.append((10, 12))
+        renderChange()
+    }
+    
+    func renderChange(){
+        for (node, row, column) in scene.gameArray{
+            if contains(a: scene.playerPosition, v: (row, column)){
+                node.fillColor = SKColor.cyan
+            } else{
+                node.fillColor = SKColor.clear
+            }
+        }
+    }
+    
+    func contains(a : [(Int, Int)], v : (Int, Int)) -> Bool {
+        let (c1, c2) = v
+        for (v1, v2) in a {
+            if v1 == c1 && v2 == c2 {
+                return true
+            }
+        }
+        return false
+    }
 }
--- Snake/GameScene.swift
+++ Snake/GameScene.swift
@@ -25,7 +25,7 @@ class GameScene: SKScene {
         
     override func didMove(to view: SKView) {
         initializeMenu()
-        game = GameManager()
+        game = GameManager(scene : self)
         initializeGameView()
     }
     
@@ -104,6 +104,7 @@ class GameScene: SKScene {
             self.currentScore.isHidden = false
             self.gameBackGround.run(SKAction.scale(to: 1, duration: 0.4))
             self.currentScore.run(SKAction.scale(to: 1, duration: 0.4))
+            self.game.initGame()
         }
     }
     
